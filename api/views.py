5391783dcb503981d654097e006ccef8a817d978
Krishna Sapkota <krrish1061@gmail.com>
2023-04-30 18:15:27 +0545

    testing data send

--- api/views.py
+++ api/views.py
@@ -23,6 +23,9 @@ def sensor_value_list(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "POST":
         content = request.body.decode("utf-8")
+        print(content)
+        print("data from next line")
+        print(request.META)
         data = set_data(content)
         # api_key=Zp8OzUK5VXC2IncqDw5GTbDSZCbLVWSR&field1=20.3&field2=45.8&field3=1&board=1&gpio=36&cboard=3
 
