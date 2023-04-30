8690936af1545993e2bc25e0aadba790638bfeac
Krishna Sapkota <krrish1061@gmail.com>
2023-04-30 18:41:33 +0545

    changing views

--- api/views.py
+++ api/views.py
@@ -23,13 +23,13 @@ def sensor_value_list(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "POST":
         content = request.body.decode("utf-8")
-        print(content)
+        # print(content)
         print("data from next line")
-        print(request.META)
+        # print(request.META)
         data = set_data(content)
         # api_key=Zp8OzUK5VXC2IncqDw5GTbDSZCbLVWSR&field1=20.3&field2=45.8&field3=1&board=1&gpio=36&cboard=3
 
         serializer = SensorValueSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_200_OK)
